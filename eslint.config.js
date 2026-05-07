6f954fa292e67b4951f5ab442740fff91b3b767e
Bee <bee@aaas.io>
2026-05-07 20:51:17 +0800

    [bee] AAAS-394: fix lint overrides and tsconfig coverage

--- eslint.config.js
+++ eslint.config.js
@@ -59,6 +59,7 @@ export default tseslint.config(
       '@typescript-eslint/unbound-method': 'off',
       '@typescript-eslint/no-unnecessary-type-assertion': 'off',
       '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
 
